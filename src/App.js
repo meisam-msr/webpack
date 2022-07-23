5b37619ad6aaaf874823f022c48f3bcbd5495cc3
meisam <maghsoodiradmeisam12@gmail.com>
2022-07-23 12:04:35 +0430

    add css-loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
+import logoImg from "./pexel.jpg";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="App">
+      <h1>React App!</h1>
+      <img src={logoImg} />
+    </div>
+  );
+};
 
-export default App
+export default App;
