63db5f88bca346ba3d5ddf577f967a8b4f98acff
meisam <maghsoodiradmeisam12@gmail.com>
2022-07-23 16:37:47 +0430

    add rect-refresh plugin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
+import { Counter } from "./Counter";
 import logoImg from "./pexel.jpg";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>React App! {process.env.NODE_ENV}</h1>
+      <h1>Hi React App! {process.env.NODE_ENV}</h1>
       <h1>{process.env.NAME}</h1>
-      <img src={logoImg} />
+      <Counter />
     </div>
   );
 };
