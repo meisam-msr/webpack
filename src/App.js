d4c73fbf1578fbf6bd13750284e2785b5fece74f
meisam <maghsoodiradmeisam12@gmail.com>
2022-07-23 13:26:20 +0430

    merge configs and use env vars

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import logoImg from "./pexel.jpg";
 const App = () => {
   return (
     <div className="App">
-      <h1>React App!</h1>
+      <h1>React App! {process.env.NODE_ENV}</h1>
+      <h1>{process.env.NAME}</h1>
       <img src={logoImg} />
     </div>
   );
---
+++ webpack/webpack.common.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "./src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "..", "./build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "..", "./src/index.html"),
+    }),
+  ],
+};
